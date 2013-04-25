b07a12870f7e4f3c44d02932b5fb2b157e83afe6
r3lay <r3lay08@gmail.com>
2013-04-26 01:18:35 +0200

    Update Urban.php

--- Classes/Command/Urban.php
+++ Classes/Command/Urban.php
@@ -32,65 +32,56 @@ class Urban extends \Library\IRC\Command\Base {
      *
      * 
      */
-	public function urban_lookup($term) 
-				{
-					$url = 'http://api.urbandictionary.com/v0/define?term='.$term;
-					$ch = curl_init();
-					curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-					curl_setopt($ch, CURLOPT_URL, $url);
-					$data = curl_exec($ch);
-					curl_close($ch);
-					$results = json_decode($data);
-					return $results;
-				}
+     public function urban_lookup($term) 
+     {
+	$url = 'http://api.urbandictionary.com/v0/define?term='.$term;
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_URL, $url);
+	$data = curl_exec($ch);
+	curl_close($ch);
+	$results = json_decode($data);
+	return $results;
+     }
 
     /**
      * Sends the arguments to the channel, like say from a user.
      *
      * 
      */
-    public function command() {
-		$urbanTerm = implode(' ', $this->arguments);
-		$urbanTerm = preg_replace('/\s\s+/', ' ', $urbanTerm);
-		$urbanTerm = trim($urbanTerm);
-		$urbanTerm = urlencode($urbanTerm);
+    public function command() 
+    {
+	$urbanTerm = implode(' ', $this->arguments);
+	$urbanTerm = preg_replace('/\s\s+/', ' ', $urbanTerm);
+	$urbanTerm = trim($urbanTerm);
+	$urbanTerm = urlencode($urbanTerm);
 		
 		
-			if (!strlen($urbanTerm))
-			{
-            $this->say(sprintf('Enter search term. (Usage: !urban [search term])'));
-            return;
-			}
+	if (!strlen($urbanTerm))
+	{
+          $this->say(sprintf('Enter search term. (Usage: !urban [search term])'));return;
+	}
 			
 				
 			 
-				if(!isset($urbanTerm)) die('no term specified');
+	if(!isset($urbanTerm)) die('no term specified');
+	 
+	$words = $this->urban_lookup($urbanTerm);
+	 
+	if($words->result_type == 'no_results')
+	{
+	   $this->say('Even urban dictionary hasn\'t heard of that, you sicko');
+	   die();
+	}
 				 
-				$words = $this->urban_lookup($urbanTerm);
 				 
-				if($words->result_type == 'no_results')
-				{
-					$this->say('Even urban dictionary hasn\'t heard of that, you sicko');
-					die();
-				}
-				 
-				 
-				/*if ($type == "d")
-				{
-					$keys = array('definition');
-				}
-				else if(($type == "e"))
-				{
-					$keys = array('example');
-				}
-				*/
-				$keys = array('definition');
-				
-				foreach($keys as $key)
-				{
-					$this->say("Urban Dictionary - ".$urbanTerm);
-					$this->say($words->list[0]->$key);
-				}
+	$keys = array('definition');
+	
+	foreach($keys as $key)
+	{
+	   $this->say("Urban Dictionary - ".$urbanTerm);
+	   $this->say($words->list[0]->$key);
+	}
 		
 		
 		
